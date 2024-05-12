697284d0ed322ef6b346a79cc88cf9e09f3d2dd1
Cfomodz <david.ashby.lds@gmail.com>
2024-05-12 14:46:10 -0600

    Update credentials.py
    exception handling and some refactoring

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
-
 import os
 from dotenv import load_dotenv
 from modules import terminal
+from config import DEBUG_MODE
 
 class Credentials:
     app_key = None
@@ -16,15 +16,21 @@ class Credentials:
         cls.app_key = os.getenv('APP_KEY')
         cls.app_secret = os.getenv('APP_SECRET')
         cls.callback_url = os.getenv('CALLBACK_URL')
-        cls.validate_credentials()
+        if not cls.validate_credentials():
+            if DEBUG_MODE:
+                terminal.color_print.error("Failed to load or validate credentials.")
+            quit()
+        terminal.color_print.info("Credentials loaded and validated successfully.")
 
     @classmethod
     def validate_credentials(cls):
         if not cls.app_key or not cls.app_secret:
             terminal.color_print.error("No app key or app secret found. Please check your .env file.")
-            quit()
-        if len(cls.app_key) != 32 or len(cls.app_secret) != 16:
-            terminal.color_print.error("Invalid app key or app secret. Please check your credentials.")
-            quit()
-
-        terminal.color_print.info("Credentials loaded successfully.")
+            return False
+        if len(cls.app_key) != 32:
+            terminal.color_print.error(f"Invalid app key length: {len(cls.app_key)}. Expected length is 32.")
+            return False
+        if len(cls.app_secret) != 16:
+            terminal.color_print.error(f"Invalid app secret length: {len(cls.app_secret)}. Expected length is 16.")
+            return False
+        return True
