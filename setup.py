04a865d726e89f37198f732844a852d0b9b4de94
David <david.ashby.lds@gmail.com>
2024-06-26 22:03:58 -0400

    ready for live

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pythonic_schwab_api',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=["requests", "python-dotenv", "websockets"],
     author='Cfomodz',
